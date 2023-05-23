74b251f4e3aabe773b59c5786ceef262281e26b7
Thalyson <thalyon.silva@ifce.edu.br>
2023-05-23 19:28:56 -0300

    Adição do BoxController

--- src/main/java/com/saper/boxbackend/controller/BoxController.java
+++ src/main/java/com/saper/boxbackend/controller/BoxController.java
@@ -5,10 +5,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/client")
-public class ClientController {
+@RequestMapping("/box")
+public class BoxController {
+
     @GetMapping
-    public String hello(){
-        return "Hello Saper";
+    public String getAll(){
+        return "Get boxes";
     }
 }
