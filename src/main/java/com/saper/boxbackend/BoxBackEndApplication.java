977f0c97d0c9c70e3b7693167d868b04301bcbc9
Thalyson <thalyon.silva@ifce.edu.br>
2023-05-23 19:17:41 -0300

    Initial commit

---
+++ src/main/java/com/saper/boxbackend/BoxBackEndApplication.java
@@ -0,0 +1,11 @@
+package com.saper.boxbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoxBackEndApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(BoxBackEndApplication.class, args);
+	}
+}
---
+++ src/test/java/com/saper/boxbackend/BoxBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saper.boxbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoxBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
