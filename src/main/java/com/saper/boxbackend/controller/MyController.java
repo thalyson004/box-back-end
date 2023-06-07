5a207a34b08ba0868cf5e083b812e11c47d56c86
Thalyson <thalyon.silva@ifce.edu.br>
2023-06-07 19:33:33 -0300

    Adição de endpoint para retornar o client logado.

--- src/main/java/com/saper/boxbackend/controller/MyController.java
+++ src/main/java/com/saper/boxbackend/controller/MyController.java
@@ -1,9 +1,24 @@
 package com.saper.boxbackend.controller;
 
+import com.saper.boxbackend.dto.ClientResponseDTO;
+import com.saper.boxbackend.service.MyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/my")
 public class MyController {
+
+    @Autowired
+    MyService myService;
+
+    @GetMapping("/client")
+    public ResponseEntity<Object> getClient(){
+        return ResponseEntity.status(HttpStatus.OK).body(new ClientResponseDTO(myService.getLogged()));
+    }
 }
--- src/main/java/com/saper/boxbackend/service/MyService.java
+++ src/main/java/com/saper/boxbackend/service/MyService.java
@@ -1,7 +1,13 @@
 package com.saper.boxbackend.service;
 
+import com.saper.boxbackend.model.Client;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MyService {
+
+    public Client getLogged(){
+        return (Client) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+    }
 }
