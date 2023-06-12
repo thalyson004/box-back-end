89c4667e4c3669f6e56947b7f10e685fc5207499
Thalyson <thalyon.silva@ifce.edu.br>
2023-06-12 16:39:55 -0300

    Adição de Tratamento para erro de integridade.

--- src/main/java/com/saper/boxbackend/exception/ValidationExceptionHandler.java
+++ src/main/java/com/saper/boxbackend/exception/ValidationExceptionHandler.java
@@ -3,6 +3,7 @@ package com.saper.boxbackend.exception;
 
 import com.saper.boxbackend.dto.ErrorDTO;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -44,6 +45,20 @@ public class ValidationExceptionHandler {
         errorDTO.setStatus(HttpStatus.NOT_FOUND.toString());
         errorDTO.setError("resource not found");
         errorDTO.setMessage(exception.getMessage());
+        errorDTO.setPath(request.getRequestURI());
+        return errorDTO;
+    }
+
+    @ResponseStatus(code = HttpStatus.CONFLICT)
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ErrorDTO handleDataIntegrityViolationException(
+            DataIntegrityViolationException exception,
+            HttpServletRequest request) {
+        ErrorDTO errorDTO = new ErrorDTO();
+        errorDTO.setTimeStamp(Instant.now());
+        errorDTO.setStatus(HttpStatus.CONFLICT.toString());
+        errorDTO.setError("Database conflict");
+        errorDTO.setMessage("Could not execute statement");
         errorDTO.setPath(request.getRequestURI());
         return errorDTO;
     }
