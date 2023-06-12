880e894e0ec953955c8589ae92caeee49e3ee8a4
Thalyson <thalyon.silva@ifce.edu.br>
2023-06-12 13:44:41 -0300

    Atualizar a versão para 3.1.0

--- src/main/java/com/saper/boxbackend/config/security/SecurityConfiguration.java
+++ src/main/java/com/saper/boxbackend/config/security/SecurityConfiguration.java
@@ -3,22 +3,26 @@ package com.saper.boxbackend.config.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class SecurityConfiguration {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.httpBasic();
-        http.authorizeHttpRequests()
+        http.httpBasic(withDefaults());
+        http.authorizeHttpRequests( (authz)->authz
                 .requestMatchers(HttpMethod.POST, "/client").permitAll()
                 .requestMatchers("/my/**").hasRole("STUDENT")
-                .anyRequest().hasRole("ADMIN");
-        http.csrf().disable();
+                .anyRequest().hasRole("ADMIN"));
+        http.csrf((csrf) -> csrf.disable());
 
         return http.build();
     }
