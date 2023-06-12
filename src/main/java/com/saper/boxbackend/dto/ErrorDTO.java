c095a0e9817fdf856132446f38e41504deebc561
Thalyson <thalyon.silva@ifce.edu.br>
2023-06-12 13:56:19 -0300

    Alteração no ErrorDTO e no tratamento de exceção.

--- src/main/java/com/saper/boxbackend/dto/ErrorDTO.java
+++ src/main/java/com/saper/boxbackend/dto/ErrorDTO.java
@@ -1,15 +1,26 @@
 package com.saper.boxbackend.dto;
 
+import java.time.Instant;
+
 public class ErrorDTO {
-    public String field;
+    public Instant timeStamp;
+
+    public String status;
+
     public String error;
 
-    public ErrorDTO(String field, String error) {
-        this.field = field;
+    public String message;
+
+    public String path;
+
+    public ErrorDTO(Instant timeStamp, String status, String error, String message, String path) {
+        this.timeStamp = timeStamp;
+        this.status = status;
         this.error = error;
+        this.message = message;
+        this.path = path;
     }
 
     public ErrorDTO() {
     }
-
 }
